536d429f3a0d2e30fe349ec45dbb2a32b37d1674
Luiz Fernando <f3rn.luiz@gmail.com>
2025-11-06 15:42:31 -0300

    feat(Tailwind CSS / Fonts): configuração da base + prettier + home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', loadComponent: () => import('./pages/home/home.page').then((p) => p.HomePage) }];
---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+	selector: 'app-home',
+	templateUrl: './home.page.html',
+})
+export class HomePage {}
